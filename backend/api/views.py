7e502a860d647dcda8a80561cf1702caf473e478
apg10 <adrianlive1024@gmail.com>
2026-04-20 14:32:48 +1000

    fix: set explicit permissions for order and payment endpoints

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,4 @@
-﻿from rest_framework import generics, permissions
+from rest_framework import generics, permissions
 from core import models
 from core.models import Product, Category, Brand
 from . import serializers
